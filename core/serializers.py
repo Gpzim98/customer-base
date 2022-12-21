137cce4ef7deb0333fcc47b6a4d85b36272e9e1b
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-12-21 00:33:39 +0000

    Nested Serializers

--- core/serializers.py
+++ core/serializers.py
@@ -7,10 +7,22 @@ from .models import (
 )
 
 
+class DataSheetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataSheet
+        fields = ['id', 'description', 'historical_data']
+
+
+class ProfessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profession
+        fields = ['id', 'description', 'status']
+
+
 class CustomerSerializer(serializers.ModelSerializer):
     num_professions = serializers.SerializerMethodField()
-    data_sheet = serializers.PrimaryKeyRelatedField(read_only=True)
-    professions = serializers.StringRelatedField(many=True)
+    data_sheet = DataSheetSerializer()
+    professions = ProfessionSerializer(many=True)
     document_set = serializers.StringRelatedField(many=True)
 
     class Meta:
@@ -23,18 +35,6 @@ class CustomerSerializer(serializers.ModelSerializer):
 
     def get_data_sheet(self, obj):
         return obj.data_sheet.description
-
-
-class ProfessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profession
-        fields = ['id', 'description']
-
-
-class DataSheetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DataSheet
-        fields = ['id', 'description', 'historical_data']
 
 
 class DocumentSerializer(serializers.ModelSerializer):
